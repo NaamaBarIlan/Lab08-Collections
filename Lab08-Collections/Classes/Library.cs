213126b9e1d43a6441b12a5239e7ed5250d1385a
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-07-15 19:38:43 -0700

    Add project files.

---
+++ Lab08-Collections/Classes/Library.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab08_Collections.Classes
+{
+    class Library<T> : IEnumerable
+    {
+        T[] inventory = new T[10];
+        int count = 0;
+
+        /// <summary>
+        /// Add a new item to the Library List
+        /// </summary>
+        /// <param name="book">The new object that needs to be added to the list</param>
+        public void Add(T book)
+        {
+           if (count == inventory.Length)
+            {
+                Array.Resize(ref inventory, inventory.Length * 2);
+            }
+
+            inventory[count++] = book;
+        }
+
+        public T Remove(T item)
+        {
+            int quarter = count - 1;
+            int tempcount = 0;
+            T[] temp;
+            T removedBook = default(T);
+
+            if (IsAvailable(item))
+            {
+                if(count < inventory.Length / 2)
+                {
+                    temp = new T[quarter];
+                }
+                else
+                {
+                    temp = new T[inventory.Length];
+                }
+
+                for (int i = 0; i < count; i++)
+                {
+                    if (inventory[i] != null)
+                    {
+                        if (!inventory[i].Equals(item))
+                        {
+                            temp[tempcount] = inventory[i];
+
+                            tempcount++;
+                        }
+                        else
+                        {
+                            removedBook = inventory[i];
+                        }
+                    }
+                }
+                inventory = temp;
+                count--;
+            }
+            
+            return removedBook;
+        }
+
+        public int Count()
+        {
+            return count;
+        }
+
+        // Stretch goal
+        /// <summary>
+        /// This method checks to see if an item exists in the Library
+        /// </summary>
+        /// <param name="book">The book that is being searched</param>
+        /// <returns>True/False indicator if the book exists in the library</returns>
+        public bool IsAvailable(T book)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (inventory[i].Equals(book))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return inventory[i];
+            }
+        }
+
+        // Magic Don't Touch
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Lab08-Collections/Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Lab08_Collections.Classes;
+
+namespace Lab08_Collections
+{
+    class Program
+    {
+        public static Library<Book> Library { get; set; }
+
+        public static List<Book> BookBag { get; set; }
+
+        static void Main(string[] args)
+        {
+            Library = new Library<Book>();
+            BookBag = new List<Book>();
+
+            LoadBooks();
+            UserInterface();
+        }
+
+        static void UserInterface()
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine();
+
+                Console.WriteLine("Welcome to Phil's lending library!");
+                Console.WriteLine("Please pick an option: ");
+                Console.WriteLine("1) View All Books");
+                Console.WriteLine("2) Add New Book");
+                Console.WriteLine("3) Borrow a Book");
+                Console.WriteLine("4) Return a Book");
+                Console.WriteLine("5) View Book Bag");
+                Console.WriteLine("6) Exit");
+
+                string answer = Console.ReadLine();
+
+                switch (answer)
+                {
+                    case "1":
+                        Console.Clear();
+
+                        OutputBooks();
+
+                        break;
+                    case "2":
+                        Console.Clear();
+                        Console.WriteLine("Please enter the following details: ");
+                        Console.Write("Book Title: ");
+                        string title = Console.ReadLine();
+                        Console.WriteLine();
+                        Console.Write("Author First Name: ");
+                        string firstName = Console.ReadLine();
+                        Console.WriteLine();
+                    case "3":
+                        Console.Clear();
+
+                        Dictionary<int, string> books = new Dictionary<int, string>();
+                        Console.WriteLine("Which book would you like to borrow? Please only select the number.");
+                        int counter = 1;
+                        foreach (Book book in Library)
+                        {
+                            books.Add(counter, book.Title);
+                            Console.WriteLine($"{counter++}. {book.Title} - {book.Author.FirstName} {book.Author.LastName}");
+                        }
+
+                        string response = Console.ReadLine();
+                        int.TryParse(response, out int selection);
+                        books.TryGetValue(selection, out string borrowtitle);
+                        Borrow(borrowtitle);
+                        break;
+                    case "4":
+                        ReturnBook();
+                        break;
+                    case "5":
+                        Console.Clear();
+
+                        foreach (Book book in BookBag)
+                        {
+                            Console.WriteLine($"{book.Title} - {book.Author.FirstName} {book.Author.LastName}");
+                        }
+                        break;
+                    case "6":
+                        exit = true;
+                        Environment.Exit(1);
+                        break;
+                    default:
+                        Console.WriteLine("Sorry, this is an invalid option...");
+                        break;
+                }
+
+            }
+        }
+
+        static void AddABook(string title, string firstName, string lastName, int numberOfPages)
+        {
+            Book book = new Book()
+            {
+                Title = title,
+                Author = new Author() { FirstName = firstName, LastName = lastName },
+                NumberOfPages = numberOfPages,
+                Genre = Genre.Romance
+            };
+
+            Library.Add(book);
+        }
+
+        static void OutputBooks()
+        {
+            int counter = 1;
+            foreach (Book book in Library)
+            {
+                Console.WriteLine($"{counter++}.{book.Title} - {book.Author.FirstName} {book.Author.LastName}");
+            }
+        }
+
+        static void LoadBooks()
+        {
+            Book a = new Book
+            {
+                Title = "Pride and Prejudice",
+                Author = new Author() { FirstName = "Jane", LastName = "Austen" },
+                Genre = Genre.Comedy
+            };
+
+            Book b = new Book
+            {
+                Title = "1984",
+                Author = new Author() { FirstName = "George", LastName = "Orwell" },
+                Genre = Genre.Drama
+            };
+
+            Book c = new Book
+            {
+                Title = "The Hobbit",
+                Author = new Author() { FirstName = "J.R.R.", LastName = "Tolkien" },
+                Genre = Genre.Fantasy
+            };
+
+            Book d = new Book
+            {
+                Title = "The Lion, the Witch and the Wardrobe",
+                Author = new Author() { FirstName = "C.S.", LastName = "Lewis" },
+                Genre = Genre.Fantasy
+            };
+
+            Book e = new Book
+            {
+                Title = "Jane Eyre",
+                Author = new Author() { FirstName = "Charlotte", LastName = "Bronte" },
+                Genre = Genre.Drama
+            };
+
+            Library.Add(a);
+            Library.Add(b);
+            Library.Add(c);
+            Library.Add(d);
+            Library.Add(e);
+
+        }
+
+        static void ReturnBook()
+        {
+            Dictionary<int, Book> books = new Dictionary<int, Book>();
+            Console.WriteLine("Which book would you like to return?");
+            int counter = 1;
+            foreach (var item in BookBag)
+            {
+                books.Add(counter, item);
+                Console.WriteLine($"{counter++}. {item.Title} - {item.Author.FirstName} {item.Author.LastName}");
+            }
+
+            string response = Console.ReadLine();
+            int.TryParse(response, out int selection);
+            books.TryGetValue(selection, out Book returnedBook);
+            BookBag.Remove(returnedBook);
+            Library.Add(returnedBook);
+        }
+
+        public static Book Borrow(string title)
+        {
+            Book borrowedBook = null;
+            foreach (Book book in Library)
+            {
+                if (book.Title == title)
+                {
+                    borrowedBook = book;
+                    BookBag.Add(Library.Remove(borrowedBook));
+                    break;
+                }
+            }
+            return borrowedBook;
+        }
+    }
+}
